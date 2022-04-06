86a86f02d425f216af5bad97e3ad75601c6d1a23
azsilva <afranio.silva@castgroup.com.br>
2022-04-06 14:37:59 -0300

    Aplica assert para expressoes boolean

---
+++ src/test/java/br/ce/wcaquino/servicos/AssertTest.java
@@ -0,0 +1,21 @@
+package br.ce.wcaquino.servicos;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AssertTest {
+	
+	
+	@Test
+	public void test() {
+		
+		//Verificação de argumentos boleanos
+		Assert.assertTrue(true);
+		Assert.assertFalse(false);
+		
+		
+		
+		
+	}
+
+}
